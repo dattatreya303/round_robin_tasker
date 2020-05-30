263b71e0514e05982b1fb16e932f976d63fb7462
Dattatreya Mohapatra <chikumhptr@gmail.com>
2020-05-30 15:00:41 +0530

    Move logger to constants to make it available to all modules

--- Constants.py
+++ Constants.py
@@ -1,3 +1,9 @@
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 TOKEN = "958111367:AAFbNEDl0V6KJxKLq64P0zHf2YwWefo4Mxc"
 TOKEN_TEST = "1243699574:AAEMQzMsmHlyNqpueSZo9xaq6zFsGBwuV8g"
 
@@ -9,4 +15,4 @@ MYSQL_DB = "dattatreya303$round-robin-tasker-bot"
 
 NUM_TASKS_CREATED_DATA_KEY = 'bot_num_tasks_created'
 
-CANCEL_CONV_PROMPT = "Send /cancel to stop terminate this command."
+CANCEL_CONV_PROMPT = "Send /cancel to terminate this command."
