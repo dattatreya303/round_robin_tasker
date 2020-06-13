5e8ffe10bd8b6c68fce119dad397a359cc18b902
Dattatreya Mohapatra <chikumhptr@gmail.com>
2020-06-13 22:44:15 +0530

    Test 100% coverage of TaskData.who method call

--- tests/unittests/TestTaskDataMethods.py
+++ tests/unittests/TestTaskDataMethods.py
@@ -1,13 +1,32 @@
+import pytest
+
 from entities.TaskData import TaskData
 from entities.UserData import UserData
 
 
 class TestTaskDataMethods:
-    def test_who(self):
-        user_a = UserData('a')
-        user_b = UserData('b')
-        user_c = UserData('c')
-        task_data = TaskData(123, 'test_task', [user_a, user_b, user_c])
-        assert task_data.who() == user_a.user_name
-        assert task_data.who() == user_b.user_name
-        assert task_data.who() == user_c.user_name
+    @pytest.fixture(scope='module')
+    def sample_user_a(self):
+        return UserData('a')
+
+    @pytest.fixture(scope='module')
+    def sample_user_b(self):
+        return UserData('b')
+
+    @pytest.fixture(scope='module')
+    def sample_user_c(self):
+        return UserData('c')
+
+    @pytest.fixture
+    def sample_task_abc(self, sample_user_a, sample_user_b, sample_user_c):
+        return TaskData(123, 'sample_task_abc', [sample_user_a, sample_user_b, sample_user_c])
+
+    @pytest.fixture
+    def sample_task_empty(self):
+        return TaskData(321, 'sample_task_empty', [])
+
+    def test_who(self, sample_task_empty, sample_task_abc, sample_user_a, sample_user_b, sample_user_c):
+        assert sample_task_empty.who() is None
+        assert sample_task_abc.who() == sample_user_a.user_name
+        assert sample_task_abc.who() == sample_user_b.user_name
+        assert sample_task_abc.who() == sample_user_c.user_name
