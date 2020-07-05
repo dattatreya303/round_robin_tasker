5eecc3b302721db621cd8bcdd214c0735074ba64
Dattatreya Mohapatra <chikumhptr@gmail.com>
2020-07-05 22:55:16 +0530

    Add unit test suite (#23)
    * Add first unit test
    
    * Test 100% coverage of TaskData.who method call
    
    * Remove bug in UserData __eq__ method
    
    * Remove conficts between tests
    
    * Add build status badge
    
    * Update readme with correct badge url
    
    * change badge url from localhost to ngrok

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Build Status](https://0821c66e51e1.ngrok.io/buildStatus/icon?job=rrtb_tests)](https://0821c66e51e1.ngrok.io/job/rrtb_tests/)
+
 # round_robin_tasker
 A telegram bot for groups to assign a task to members in a round robin manner
---
+++ tests/unittests/TestTaskDataMethods.py
@@ -0,0 +1,68 @@
+import pytest
+
+from entities.TaskData import TaskData
+from entities.UserData import UserData
+
+
+class TestTaskDataMethods:
+    @pytest.fixture(scope='module')
+    def sample_user_a(self):
+        return UserData('a')
+
+    @pytest.fixture(scope='module')
+    def sample_user_b(self):
+        return UserData('b')
+
+    @pytest.fixture(scope='module')
+    def sample_user_c(self):
+        return UserData('c')
+
+    @pytest.fixture(scope='module')
+    def sample_user_d(self):
+        return UserData('d')
+
+    @pytest.fixture(scope='module')
+    def sample_task_abc(self, sample_user_a, sample_user_b, sample_user_c):
+        return TaskData(123, 'sample_task_abc', [sample_user_a, sample_user_b, sample_user_c])
+
+    @pytest.fixture()
+    def sample_task_abc_editable(self, sample_user_a, sample_user_b, sample_user_c):
+        return TaskData(123, 'sample_task_abc', [sample_user_a, sample_user_b, sample_user_c])
+
+    @pytest.fixture()
+    def sample_task_empty(self):
+        return TaskData(321, 'sample_task_empty', [])
+
+    def test_add_participant(self, sample_task_empty, sample_user_c, sample_user_d):
+        assert len(sample_task_empty.participants) == 0
+        sample_task_empty.add_participant(sample_user_c.user_name)
+        assert len(sample_task_empty.participants) == 1
+        assert sample_task_empty.participants[0].user_name == sample_user_c.user_name
+        sample_task_empty.add_participant(sample_user_d.user_name)
+        assert len(sample_task_empty.participants) == 2
+        assert sample_task_empty.participants[1].user_name == sample_user_d.user_name
+
+    def test_remove_participant(self, sample_task_abc_editable, sample_user_a, sample_user_b, sample_user_c):
+        sample_task_abc_editable.remove_participant(sample_user_a.user_name)
+        assert any(map(lambda x: x.user_name == sample_user_a.user_name, sample_task_abc_editable.participants)) is False
+        assert len(sample_task_abc_editable.participants) == 2
+        sample_task_abc_editable.remove_participant(sample_user_b.user_name)
+        assert any(map(lambda x: x.user_name == sample_user_a.user_name, sample_task_abc_editable.participants)) is False
+        assert len(sample_task_abc_editable.participants) == 1
+        sample_task_abc_editable.remove_participant(sample_user_c.user_name)
+        assert any(map(lambda x: x.user_name == sample_user_a.user_name, sample_task_abc_editable.participants)) is False
+        assert len(sample_task_abc_editable.participants) == 0
+
+    def test_check_participant_exists_by_name(self, sample_task_empty, sample_task_abc, sample_user_a, sample_user_b,
+                                              sample_user_c, sample_user_d):
+        assert sample_task_empty.check_participant_exists_by_name(sample_user_a.user_name) is False
+        assert sample_task_abc.check_participant_exists_by_name(sample_user_a.user_name) is True
+        assert sample_task_abc.check_participant_exists_by_name(sample_user_b.user_name) is True
+        assert sample_task_abc.check_participant_exists_by_name(sample_user_c.user_name) is True
+        assert sample_task_abc.check_participant_exists_by_name(sample_user_d.user_name) is False
+
+    def test_who(self, sample_task_empty, sample_task_abc, sample_user_a, sample_user_b, sample_user_c):
+        assert sample_task_empty.who() is None
+        assert sample_task_abc.who() == sample_user_a.user_name
+        assert sample_task_abc.who() == sample_user_b.user_name
+        assert sample_task_abc.who() == sample_user_c.user_name
